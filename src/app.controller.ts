eea8a924b584d0e5e36e93e53119bb3e646c5039
alifhs <alif.hs@outlook.com>
2023-12-12 22:25:32 +0600

    add passport authguard

---
+++ src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
+  }
+}
